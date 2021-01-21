55c5ccff8e238770a456bd7ab9109e273c035961
dongnguyen248 <dacdongdhv@gmail.com>
2021-01-21 07:47:18 +0700

    setup for project

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,8 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('post_id');
+            $table->boolean('role')->default(0);
             $table->rememberToken();
             $table->timestamps();
+
         });
     }
 
