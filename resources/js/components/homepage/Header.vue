61cc430bc3a89fda40bbf8895892d4fb3b0a1aab
dongnguyen248 <dacdongdhv@gmail.com>
2021-01-22 13:19:35 +0700

    create header

---
+++ resources/js/components/homepage/Header.vue
@@ -0,0 +1,21 @@
+<template>
+<header class="header-white-default header-white header-fixed">
+    <div class="row align-items-center">
+        <a class="brand" href="#">
+            <img :src="'./images/Mylogo.png'" alt="">
+        </a>
+        <nav class="nav-main navbar-expand-lg">
+            <nav class="nav nav-default">
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav">
+                        <li class="dropdown"><a href="#">Laravel</a></li>
+                        <li class=""><a href="#">Vuejs</a></li>
+                        <li class=""><a href="#">Nodejs</a></li>
+                        <li class=""><a href="#">About Me</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </nav>
+    </div>
+</header>
+</template>
--- resources/js/components/homepage/home.vue
+++ resources/js/components/homepage/home.vue
@@ -1,3 +1,14 @@
 <template>
-    <div>this is home page</div>
+
+    <div>
+        <header-bar></header-bar>
+        <p>this is homepage</p>
+    </div>
 </template>
+<script>
+import Header from './Header.vue'
+export default {
+     components:
+     {'header-bar':Header}
+}
+</script>
