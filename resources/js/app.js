e9d78ab39a26bca90b40fb3d7609686a9d7fe8a4
dongnguyen248 <dacdongdhv@gmail.com>
2021-01-22 10:09:28 +0700

    setup project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,48 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
+require("./bootstrap");
 
-require('./bootstrap');
+window.Vue = require('vue');
+import axios from "axios";
+import Vue from "vue";
+// import Vuex from "vuex";
+// import VueRouter from "vue-router";
+// import storeData from "./store";
+// import { routes } from "./routes";
+import homepage from "./components/homepage/home.vue";
+// import { initialize } from "./helper/general";
+// import common from "./helper/comon";
+// import numeral from 'numeral';
+// import numFormat from 'vue-filter-number-format';
+// Vue.filter('numFormat', numFormat(numeral));
+// const store = new Vuex.Store(storeData);
+// initialize(store, router);
+// Vue.mixin(common);
 
-window.Vue = require('vue').default;
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import Swal from "sweetalert2";
+const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 2500,
+    timerProgressBar: true,
+    onOpen: toast => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+    }
+});
+window.Swal = Swal;
+window.Toast = Toast;
+Vue.use(axios);
+// Vue.use(VueRouter);
+// const router = new VueRouter({
+//     // routes,
+//     mode: "history"
+// });
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
+    // router,
+    // store,
+    components: {
+        homepage
+    }
 });
